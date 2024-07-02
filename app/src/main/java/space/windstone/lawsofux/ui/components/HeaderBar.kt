0001f165bf40380b39c94b3fc5454ccdb600abb5
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-07-02 22:18:29 +0300

    feat: implement MoreMenu component

--- app/src/main/java/space/windstone/lawsofux/ui/components/HeaderBar.kt
+++ app/src/main/java/space/windstone/lawsofux/ui/components/HeaderBar.kt
@@ -1,12 +1,6 @@
 package space.windstone.lawsofux.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material.icons.outlined.Palette
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -21,10 +15,7 @@ fun HeaderBar(scrollBehavior: TopAppBarScrollBehavior) {
         title = { Text("Laws of UX", maxLines = 1, overflow = TextOverflow.Ellipsis) },
         actions = {
             IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    imageVector = Icons.Filled.MoreVert,
-                    contentDescription = "Localized description"
-                )
+                MoreMenu()
             }
         },
         scrollBehavior = scrollBehavior
