845f5e2033978eefd889dbd70c8e95fa9c9b3e6a
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-07-02 21:45:16 +0300

    feat: change BottomBar icons

--- app/src/main/java/space/windstone/lawsofux/ui/components/BottomBar.kt
+++ app/src/main/java/space/windstone/lawsofux/ui/components/BottomBar.kt
@@ -1,12 +1,27 @@
 package space.windstone.lawsofux.ui.components
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BorderStyle
+import androidx.compose.material.icons.filled.CheckBoxOutlineBlank
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.filled.Extension
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Psychology
+import androidx.compose.material.icons.filled.Square
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Tonality
+import androidx.compose.material.icons.filled.ViewInAr
+import androidx.compose.material.icons.outlined.BorderStyle
+import androidx.compose.material.icons.outlined.CheckBoxOutlineBlank
+import androidx.compose.material.icons.outlined.Circle
+import androidx.compose.material.icons.outlined.Extension
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Psychology
 import androidx.compose.material.icons.outlined.StarBorder
+import androidx.compose.material.icons.outlined.Tonality
+import androidx.compose.material.icons.outlined.ViewInAr
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -23,9 +38,9 @@ import androidx.compose.ui.tooling.preview.Preview
 fun BottomBar() {
     var selectedItem by remember { mutableIntStateOf(0) }
     val items = listOf("Heuristic", "Gestalt", "Cognitive Bias", "Principle")
-    val selectedIcons = listOf(Icons.Filled.Home, Icons.Filled.Favorite, Icons.Filled.Star, Icons.Filled.Star)
+    val selectedIcons = listOf(Icons.Filled.Psychology, Icons.Filled.ViewInAr, Icons.Filled.Extension, Icons.Filled.Circle)
     val unselectedIcons =
-        listOf(Icons.Outlined.Home, Icons.Outlined.FavoriteBorder, Icons.Outlined.StarBorder, Icons.Outlined.StarBorder)
+        listOf(Icons.Outlined.Psychology, Icons.Outlined.ViewInAr, Icons.Outlined.Extension, Icons.Outlined.Tonality)
 
     NavigationBar {
         items.forEachIndexed { index, item ->
